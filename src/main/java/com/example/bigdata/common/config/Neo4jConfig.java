409c841e791e0d98d0272896c3c8b4964e1ba0b0
likaizz <654511905@qq.com>
2018-07-30 23:10:14 +0800

    图数据库配置

--- src/main/java/com/example/bigdata/common/config/Neo4jConfig.java
+++ src/main/java/com/example/bigdata/common/config/Neo4jConfig.java
@@ -1,6 +1,7 @@
 package com.example.bigdata.common.config;
 
 import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
@@ -15,30 +16,30 @@ import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
 @EntityScan("com.example.bigdata.entity")
 @EnableNeo4jRepositories(basePackages = "com.example.bigdata.dao.neo4j")
 public class Neo4jConfig {
-    @Value("${spring.data.neo4j.uri}")
-    private  String URI;
-    @Value("${spring.data.neo4j.username}")
-    private  String UAERNAME;
-    @Value("${spring.data.neo4j.password}")
-    private  String PASSWORD;
-    @Bean("neo4jConfiguration")
-    public org.neo4j.ogm.config.Configuration configuration() {
-        org.neo4j.ogm.config.Configuration configuration = new org.neo4j.ogm.config.Configuration.Builder()
-                .uri(URI)
-                .credentials(UAERNAME, PASSWORD)
-                .build();
-        return configuration;
-    }
+//    @Value("${spring.data.neo4j.uri}")
+//    private  String URI;
+//    @Value("${spring.data.neo4j.username}")
+//    private  String UAERNAME;
+//    @Value("${spring.data.neo4j.password}")
+//    private  String PASSWORD;
+//    @Bean("neo4jConfiguration")
+//    public org.neo4j.ogm.config.Configuration configuration() {
+//        org.neo4j.ogm.config.Configuration configuration = new org.neo4j.ogm.config.Configuration.Builder()
+//                .uri(URI)
+//                .credentials(UAERNAME, PASSWORD)
+//                .build();
+//        return configuration;
+//    }
     @Bean({/*"transactionManager",*/"neo4jTransactionManager"})
-    public Neo4jTransactionManager transactionManager() throws Exception {
+    public Neo4jTransactionManager transactionManager(@Autowired SessionFactory sessionFactory) throws Exception {
         //return new JtaTransactionManagerFactoryBean();
-        return new Neo4jTransactionManager(sessionFactory());
+        return new Neo4jTransactionManager(sessionFactory);
     }
-    @Bean({"sessionFactory",/*"getSessionFactory",*/"neo4jSessionFactory"})
-    public SessionFactory sessionFactory() {
-        SessionFactory sessionFactory= new SessionFactory(configuration(),"com.example.bigdata.dao.neo4j");//"com.example.mongoapp.dao.neo4j""com.example.mongoapp.entity"
-        return sessionFactory;
-    }
+//    @Bean({"sessionFactory",/*"getSessionFactory",*/"neo4jSessionFactory"})
+//    public SessionFactory sessionFactory() {
+//        SessionFactory sessionFactory= new SessionFactory(configuration(),"com.example.bigdata.dao.neo4j");//"com.example.mongoapp.dao.neo4j""com.example.mongoapp.entity"
+//        return sessionFactory;
+//    }
 
 
 }
